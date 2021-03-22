ea0d99b023e5e8d6e6a779ec20ff1e9377a1c0e3
Mike Hemberger <mike@thestizmedia.com>
2021-03-22 17:02:25 -0400

    dependency install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7384e1ad9e82d546e5a36f283244e8477e377980',
+    'name' => 'maithemewp/mai-sleek',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65570a1eb966bb9adfc9d9e521357da080fbdc5a',
+    ),
+    'collizo4sky/persist-admin-notices-dismissal' => 
+    array (
+      'pretty_version' => '1.4.4',
+      'version' => '1.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '900739eb6b0ec0210465f5983a6d4e0e420289e4',
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ab01c60c49a9f20a50d59adf329715145cb311ea',
+    ),
+    'maithemewp/mai-sleek' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7384e1ad9e82d546e5a36f283244e8477e377980',
+    ),
+  ),
+);
